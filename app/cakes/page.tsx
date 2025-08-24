52bf2f27a3724bb7e5dd66c5dbba3e0da12687a8
ashuushii <xshuushii@gmail.com>
2025-08-24 03:34:37 +0100

    a

--- app/cakes/page.tsx
+++ app/cakes/page.tsx
@@ -10,8 +10,7 @@ export default function CupcakesPage() {
   const cakeCategories = [
     {
       title: "",
-      description:
-        "",
+      description: "",
       cakes: [
         {
           name: "Classic Vanilla",
